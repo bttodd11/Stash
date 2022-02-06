ea982ae4750f264d21d6db76b5dc02cfecc138f3
Brian <BTodd@Brians-MacBook-Pro.local>
2022-02-05 22:31:50 -0600

    Only added login page to components

---
+++ components/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const loginScreen = StyleSheet.create({
+    fontSize: 90
+});
+
+const LoginPage = () => {
+
+    return (
+        <View style={loginScreen}>
+            <Text>Test View </Text>
+
+        </View>
+    );
+}
+
+export default LoginPage;
